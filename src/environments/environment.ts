a845aa9cb0e8a560e917b7c186154d7f74a95ac5
YamenAmjed22 <yamenamjed22@gmail.com>
2025-04-30 01:30:41 -0900

    back to what i need

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,3 @@
-// export const environment = {
-//     apiUrl: "http://localhost:8080/api"
-// };
-
-const protocol = window.location.protocol;
-const hostname = window.location.hostname;
-
-let port = '';
-if (window.location.port != '') {
-    port = ':' + window.location.port;
-}
-
-
 export const environment = {
-  apiUrl: `${protocol}//${hostname}${port}/api`
+    apiUrl: "http://localhost:8080/api"
 };
